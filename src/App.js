ab62b07370cb76e4596a2de0055efc63a7b7d7c8
cosylily <syafiqah.dayana1205@gmail.com>
2025-03-08 12:28:48 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header>Weather App</header>
+      <div className="container">
+        <header>Weather App</header>
+        <footer>
+          This project is coded by{" "}
+          <a href="https://github.com/cosylily" target="blank">
+            Hikari{" "}
+          </a>
+          and is open-sourced in{" "}
+          <a href="https://github.com/cosylily/react-astro-lily" target="blank">
+            Git Hub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
